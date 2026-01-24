dc9824bbf8620cb96e779b10f21ed2d28599dc92
Amin007 Ledang <helmi007@yahoo.com>
2026-01-24 22:58:58 +0800

    menyusun balik koding untuk tatasusunan yang lebih kompleks

--- contoh-chatgpt.php
+++ contoh-chatgpt.php
@@ -3,116 +3,115 @@
 require 'fungsi_global.php';
 require 'bujang.php';
 #--------------------------------------------------------------------------------------------------
-diatasDaa('Jenis Bisnes Di 7 ADA SOLUTION');
-#--------------------------------------------------------------------------------------------------
 # Senarai laluan yang sah (route mapping)
 #--------------------------------------------------------------------------------------------------
 $folder = ['template/amin007/','template/amin007/claude/','template/amin007/grok/',
 'template/amin007/chatgpt/'];
 $folderApa = $folder[3];
 #--------------------------------------------------------------------------------------------------
-$laluanSah = [
-	/*'buka/kedai/komputer' => $folderApa . 'kedai_komputer.php',
-	'buka/kedai/perisian' => $folderApa . 'kedai_perisian.php',
-	'buka/kedai/ict' => $folderApa . 'kedai_ict.php',
-	'buka/kedai/buku-alat-tulis' => $folderApa . 'kedai_buku-alat-tulis.php',
-	'buka/kedai/kuih' => $folderApa . 'kedai_kuih.php',//*/
-	'tawar/khidmat/cetak' => $folderApa . 'khidmat_cetak_v01.php',
-	'hubungi/whatsapp' => $folderApa . 'hubungi_whatsapp.php',
-	'hubungi/facebook' => $folderApa . 'hubungi_facebook.php',
-	'hubungi/instagram' => $folderApa . 'hubungi_instagram.php',
-];
-#--------------------------------------------------------------------------------------------------
-/*
-[0] = url pada pautan pelayar web
-[1] = pautan fail ke folder
-[2] = tajuk utama
-[3] = keterangan tajuk utama
-[4] = icon bootstrap (BI) atau font awesome(FA)
-//*/
 $pautanSah = [
 	# produk
-/*	['buka/kedai/komputer', $folderApa . 'kedai_komputer.php',
-	'Kedai Komputer, Peralatan dan Aksesori Komputer',
-	'Pelbagai jenama komputer riba, komputer meja, dan aksesori komputer terkini',
-	'bi bi-laptop-fill'],
-	['buka/kedai/perisian' => $folderApa . 'kedai_perisian.php',
-	'Kedai Perisian dan Aplikasi Mudah Alih',
-	'Pelbagai perisian berlesen tulen untuk kegunaan peribadi dan korporat',
-	'bi bi-code-square'],
-	['buka/kedai/ict' => $folderApa . 'kedai_ict.php',
-	'Kedai Peralatan Telekomunikasi dan Elektronik',
-	'Keperluan peribadi dan perniagaan.',
-	'bi bi-router-fill'],
-	['buka/kedai/buku-alat-tulis' => $folderApa . 'kedai_buku-alat-tulis.php',
-	'Kedai Buku dan Alat Tulis',
-	'Koleksi buku terkini dari pelbagai kategori dan pelbagai alat tulis berkualiti',
-	'bi bi-book-fill'],
-	['buka/kedai/kuih' => $folderApa . 'kedai_kuih.php',
-	'Kedai Produk Bakeri dan Konfeksi',
-	'Pelbagai jenis kuih tradisional, kuih raya, kek, dan produk bakeri yang lazat dan segar',
-	'bi bi-cake2-fill'],
-	/* khidmat
-	[0] = url pada pautan pelayar web
-	[1] = pautan fail ke folder
-	[2] = tajuk utama
-	[3] = keterangan tajuk utama
-	[4] = icon bootstrap (BI) atau font awesome(FA) //*
-	['tawar/khidmat/aturcara', $folderApa . 'khidmat_aturcara_v01.php',
-	'Pengaturcaraan & Aplikasi',
-	'Pembangunan aplikasi web & mudah alih menggunakan teknologi moden.',
-	'fa-solid fa-code fa-2x mb-3'],
-	['tawar/khidmat/latihan', $folderApa . 'khidmat_latihan_v01.php',
-	'Latihan ICT Masa Hadapan',
-	'Latihan komputer dan teknologi untuk individu serta organisasi.',
-	'fa-solid fa-laptop-code fa-2x mb-3'],
-	['tawar/khidmat/rnd_ai', $folderApa . 'khidmat_rnd_ai_v01.php',
-	'Penyelidikan dan Pembangunan & AI',
-	'Pembangunan penyelesaian AI dan automasi untuk pelbagai industri.',
-	'fa-solid fa-microchip fa-2x mb-3'],
-	['tawar/khidmat/nasihat', $folderApa . 'khidmat_nasihat_v01.php',
-	'Khidmat Nasihat Perisian',
-	'Nasihat pembangunan sistem, integrasi dan penyelesaian teknologi.',
-	'fa-solid fa-lightbulb fa-2x mb-3'],
-	['tawar/khidmat/penulisan', $folderApa . 'khidmat_penulisan_v01.php',
-	'Penulisan Profesional',
-	'Penulisan bebas dengan penggunaan AI, automasi dan analisis data.',
-	'fa-solid fa-pen-nib fa-2x mb-3'],//*/
-	['tawar/khidmat/cetak', $folderApa . 'khidmat_cetak_v01.php',
-	'Cetak Atas Talian',
-	'Cetak Atas Permintaan',
-	'fa-solid fa-print fa-2x mb-3'],
-	/* hubungi
-	[0] = url pada pautan pelayar web
-	[1] = pautan fail ke folder
-	[2] = tajuk utama
-	[3] = keterangan tajuk utama
-	[4] = icon bootstrap (BI) atau font awesome(FA) //*/
-	/*['hubungi/whatsapp', $folderApa . 'hubungi_whatsapp.php',
-	'WhatsApp','Media Sosial Kami',
-	'bi bi-whatsapp'],
-	['hubungi/facebook', $folderApa . 'hubungi_facebook.php',
-	'Facebook','Media Sosial Kami',
-	'bi bi-facebook'],
-	['hubungi/instagram', $folderApa . 'hubungi_instagram.php',
-	'Instagram','Media Sosial Kami',
-	'bi bi-instagram'],//*/
+/*	'buka/kedai/komputer' => [
+		'fail' => $folderApa . 'kedai_komputer.php',
+		'tajuk' => 'Kedai Komputer, Peralatan dan Aksesori Komputer',
+		'keterangan' => 'Pelbagai jenama komputer riba, komputer meja, dan aksesori komputer terkini',
+		'ikon' => 'bi bi-laptop-fill'
+	],
+	'buka/kedai/perisian' => [
+		'fail' => $folderApa . 'kedai_perisian.php',
+		'tajuk' => 'Kedai Perisian dan Aplikasi Mudah Alih',
+		'keterangan' => 'Pelbagai perisian berlesen tulen untuk kegunaan peribadi dan korporat',
+		'ikon' => 'bi bi-code-square'
+	],
+	'buka/kedai/ict' => [
+		'fail' => $folderApa . 'kedai_ict.php',
+		'tajuk' => 'Kedai Peralatan Telekomunikasi dan Elektronik',
+		'keterangan' => Keperluan peribadi dan perniagaan.',
+		'ikon' => 'bi bi-router-fill'
+	],
+	'buka/kedai/buku-alat-tulis' => [
+		'fail' => $folderApa . 'kedai_buku-alat-tulis.php',
+		'tajuk' => 'Kedai Buku dan Alat Tulis',
+		'keterangan' => 'Koleksi buku terkini dari pelbagai kategori dan pelbagai alat tulis berkualiti',
+		'ikon' => 'bi bi-book-fill'
+	],
+	'buka/kedai/kuih' => [
+		'fail' => $folderApa . 'kedai_kuih.php',
+		'tajuk' => 'Kedai Produk Bakeri dan Konfeksi',
+		'keterangan' => 'Pelbagai jenis kuih tradisional, kuih raya, kek, dan produk bakeri yang lazat dan segar',
+		'ikon' => 'bi bi-cake2-fill'
+	],
+	# khidmat
+	'tawar/khidmat/aturcara' => [
+		'fail' => $folderApa . 'khidmat_aturcara_v01.php',
+		'tajuk' => 'Pengaturcaraan & Aplikasi',
+		'keterangan' => 'Pembangunan aplikasi web & mudah alih menggunakan teknologi moden.',
+		'ikon' => 'fa-solid fa-code fa-2x mb-3'
+	],
+	'tawar/khidmat/latihan' => [
+		'fail' => $folderApa . 'khidmat_latihan_v01.php',
+		'tajuk' => 'Latihan ICT Masa Hadapan',
+		'keterangan' => 'Latihan komputer dan teknologi untuk individu serta organisasi.',
+		'ikon' => 'fa-solid fa-laptop-code fa-2x mb-3'
+	],
+	'tawar/khidmat/rnd_ai' => [
+		'fail' => $folderApa . 'khidmat_rnd_ai_v01.php',
+		'tajuk' => 'Penyelidikan dan Pembangunan & AI',
+		'keterangan' => 'Pembangunan penyelesaian AI dan automasi untuk pelbagai industri.',
+		'ikon' => 'fa-solid fa-microchip fa-2x mb-3'
+	],//*/
+	'tawar/khidmat/nasihat' => [
+		'fail' => $folderApa . 'khidmat_nasihat_v00.php',
+		'tajuk' => 'Khidmat Nasihat Perisian',
+		'keterangan' => 'Nasihat pembangunan sistem, integrasi dan penyelesaian teknologi.',
+		'ikon' => 'fa-solid fa-lightbulb fa-2x mb-3'
+	],
+	/*'tawar/khidmat/penulisan' => [
+		'fail' => $folderApa . 'khidmat_penulisan_v01.php',
+		'tajuk' => 'Penulisan Profesional',
+		'keterangan' => 'Penulisan bebas dengan penggunaan AI, automasi dan analisis data.',
+		'ikon' => 'fa-solid fa-pen-nib fa-2x mb-3'
+	],//*/
+	'tawar/khidmat/cetak' => [
+		'fail' => $folderApa . 'khidmat_cetak_v01.php',
+		'tajuk' => 'Cetak Atas Talian',
+		'keterangan' => 'Cetak Atas Permintaan',
+		'ikon' => 'fa-solid fa-print fa-2x mb-3',
+	],
+	'hubungi/whatsapp' => [
+		'fail' => $folderApa . 'hubungi_whatsapp.php',
+		'tajuk' => 'WhatsApp',
+		'keterangan' => 'Media Sosial Kami',
+		'ikon' => 'bi bi-whatsapp',
+	],
+	'hubungi/facebook' => [
+		'fail' => $folderApa . 'hubungi_facebook.php',
+		'tajuk' => 'Facebook',
+		'keterangan' => 'Media Sosial Kami',
+		'ikon' => 'bi bi-facebook'
+	],
+	'hubungi/instagram' => [
+		'fail' => $folderApa . 'hubungi_instagram.php',
+		'tajuk' => 'Instagram',
+		'keterangan' => 'Media Sosial Kami',
+		'ikon' => 'bi bi-instagram'
+	],//*/
 ];
 #--------------------------------------------------------------------------------------------------
 // Dapatkan URI semasa dan bersihkan
 $uri = ltrim(parse_url($_SERVER['QUERY_STRING'], PHP_URL_PATH), '?/');
+$failPautan = $pautanSah[$uri]['fail'] ?? $folderApa . 'readme.php';
+$tajuk = $pautanSah[$uri]['tajuk'] ?? 'Utama';
 #--------------------------------------------------------------------------------------------------
-$failMd = $laluanSah[$uri] ?? $folderApa . 'readme.php';
-#--------------------------------------------------------------------------------------------------
-/*semakPembolehubah($uri,'uri');
-semakPembolehubah($laluanSah,'laluanSah[]',0);
-semakPembolehubah($laluanSah[$uri],'laluanSah['.$uri.']',0);
-semakPembolehubah($failMd,'failMd');//*/
-#--------------------------------------------------------------------------------------------------
-if ($uri !== '' && !array_key_exists($uri, $laluanSah))
+if ($uri !== '' && !array_key_exists($uri, $pautanSah))
 {
     http_response_code(404);
 }
+#--------------------------------------------------------------------------------------------------
+/*semakPembolehubah($uri,'uri');
+semakPembolehubah($pautanSah,'pautanSah[]',0);
+semakPembolehubah($pautanSah[$uri],'pautanSah['.$uri.']',0);
+semakPembolehubah($failPautan,'failPautan');//*/
 #--------------------------------------------------------------------------------------------------
 # kod yang boleh digunakan sama di semua kedai.
 $classIcon[] = '<i class="bi bi-door-open"></i>';
@@ -123,10 +122,9 @@ $pilihIcon = $classIcon[0];
 $kembaliKePangkalJalan = $pilihIcon . 'Kembali ke Halaman Utama';
 $kembaliKePangkalJalan2 = $classIcon[3] . 'Kembali ke Halaman Utama';
 #--------------------------------------------------------------------------------------------------
-# sumber : https://stackoverflow.com/questions/72263107/how-do-i-use-markdown-it-without-node-js
-//echo '<' . $kodHTML . ' class="container markdown">';
-require $failMd; // Markdown akan dipaparkan sebagai teks biasa
-//echo '</' . $kodHTML . '><!-- / class="markdown" -->';
+diatasDaa('Bisnes:' . $tajuk);
+#--------------------------------------------------------------------------------------------------
+require $failPautan;
 #--------------------------------------------------------------------------------------------------
 ?>
 <hr>
