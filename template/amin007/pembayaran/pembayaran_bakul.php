899721bb9579f87c29d0a6919c551be83e791a31
Amin007 Ledang <helmi007@yahoo.com>
2026-02-28 21:58:00 +0800

    1. hanya terima harga dari halaman lain dan kodProduk 2. tambah nama, no tel, email dan nota tambahan

--- template/amin007/pembayaran/pembayaran_bakul.php
+++ template/amin007/pembayaran/pembayaran_bakul.php
@@ -9,7 +9,7 @@ $papar = [
 $kelas = 'btn-outline-info';
 $pautan = [
 	'pembayaran/semakpesanan',
-	'pembayaran/proses',
+	//'pembayaran/proses',
 	'pembayaran/status',
 ];
 #--------------------------------------------------------------------------------------------------
@@ -31,15 +31,49 @@ $pautan = [
 <!-- ========================================================================================== -->
 <div class="container my-5">
 	<div class="card">
-	<div class="card-body">
+	<div class="card-header">
 		<h2 class="card-title"><i class="<?php echo $papar['ikon'] ?>"></i>
 		<?php echo $papar['tajuk'] ?></h5>
 		<p class="card-text"><?php echo $papar['keterangan'] ?></p>
+	</div><!-- / class="card-header" -->
+	<div class="card-body">
+		<form method="POST" action="?/pembayaran/status">
+		<!-- ================================================================================== -->
+		<input type="hidden" name="borang[kodProduk]" value="khidmat002">
+		<input type="hidden" name="borang[harga]" value="3.00" readonly>
+		<input type="hidden" name="borang[kuantiti]" value="1">
+		<!-- ================================================================================== -->
+		<div class="mb-3">
+			<label for="nama" class="form-label">Nama:</label>
+			<input type="text" class="form-control" placeholder="Nama Anda"
+			name="borang[nama]">
+		</div><!-- / class="mb-3" -->
+		<div class="mb-3">
+			<label for="notel" class="form-label">No Tel:</label>
+			<input type="text" class="form-control" placeholder="6xxxxxxxx"
+			name="borang[notel]">
+		</div><!-- / class="mb-3" -->
+		<div class="mb-3">
+			<label for="email" class="form-label">Email:</label>
+			<input type="text" class="form-control" placeholder="nama@email.apa"
+			name="borang[email]">
+		</div><!-- / class="mb-3" -->
+		<div class="mb-3">
+			<label for="email" class="form-label">Nota:</label>
+			<textarea class="form-control" name="borang[nota] rows="4"
+			placeholder="Sila tulis nota ulasan di sini..."
+			></textarea>
+		</div><!-- / class="mb-3" -->
+		<div class="mb-3">
+			<input type="reset" class="btn btn-secondary" data-bs-dismiss="modal" value="Batal">
+			<input type="submit" class="btn btn-success" value="Sahkan Pembelian">
+		</div><!-- / class="mb-3" -->
+		</form>
 	</div><!-- / class="card-body" -->
 	<div class="card-footer">
 		<?php semakPembolehubah($_POST,'semak data POST',0);
 		?>
-	</div>
+	</div><!-- / class="card-footer" -->
 	</div><!-- / class="card" -->
 </div><!-- / class="container my-5" -->
 <!-- ========================================================================================== -->
